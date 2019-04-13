5823a0fc66c5ae81d6bd94b735c7c1e6f7fe5164
John <1101648204@qq.com>
2019-04-13 22:03:08 +0800

    正则表达式

--- src/main/java/com/zzx/husky/controller/ZRGJController.java
+++ src/main/java/com/zzx/husky/controller/ZRGJController.java
@@ -10,14 +10,16 @@ public class ZRGJController {
 
     public static void main(String[] args) {
         String s = "2012年2月4日，发自95535：您的尾号2354的银行卡于12:21在ATM机支出23元【中国银行】" +
-                "2012年2月4日，发自95535：您的尾号2354的银行卡于12:21在ATM机支出23元【中国银行】";
+                "2012年2月24日，发自95535：您的尾号2354的银行卡于12:21在ATM机支出23元【中国】";
 
-        Pattern pattern = Pattern.compile("(?<date>\\d+年\\d+月\\d+日)(.*?)(?<phone>\\d+)(.*?)(?<card>\\d+)(.*)(?<inout>(收入)|(支出))(?<num>\\d+)元");
+        Pattern pattern = Pattern.compile("(?<date>\\d+年\\d+月\\d+日)(.*?)(?<phone>\\d+)(.*?)(?<card>\\d+)(.*?)(?<inout>(收入)|(支出))(?<num>\\d+)元【(?<bank>.*?)】");
 
         Matcher matcher = pattern.matcher(s);
         while (matcher.find()){
             String date = matcher.group("date");
-
+            String bank=matcher.group("bank");
+            System.out.println(date);
+            System.out.println(bank);
         }
 
     }
